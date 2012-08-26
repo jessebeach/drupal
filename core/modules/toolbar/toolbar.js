aa262a7ea76873a95165c266a0af48e5e759acfa
J. Renée Beach <splendidnoise@gmail.com>
2012-08-26 14:48:42 -0400

    Issue #1137920 by jessebeach: The admin tray now slides side to side.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- core/modules/toolbar/toolbar.js
+++ core/modules/toolbar/toolbar.js
@@ -133,6 +133,7 @@ Drupal.TraySlider = function ($tray, $toggle) {
   this.$toggle = $toggle;
   this.state;
   this.width;
+  this.maxWidth;
   // Init the object.
   this.init.apply(this, arguments);
 };
@@ -141,8 +142,14 @@ Drupal.TraySlider = function ($tray, $toggle) {
  */
 Drupal.TraySlider.prototype.init = function () {
   this.state = 'closed';
-  this.collapse();
-  
+  this.maxWidth = 200;
+  this.width = this.getWidth();
+  // Place the menu off screen.
+  this.$tray.css({
+    'width': this.width,
+    'left': this.width * -1
+  });
+  // Add a click handler to the toggle.
   this.$toggle
   .on('click.DrupalToolbar', $.proxy(this, 'toggle'));
   // Register for offsettopchange events.
@@ -170,21 +177,35 @@ Drupal.TraySlider.prototype.toggle = function (event) {
  *
  */
 Drupal.TraySlider.prototype.expand = function (event) {
-  this.$tray.slideDown();
+  this.width = this.getWidth();
+  this.$tray.animate({
+    'width': this.width,
+    'left': 0
+  });
+  $('body').animate({
+    'padding-left': this.width
+  });
   this.state = 'open';
 };
 /**
  *
  */
 Drupal.TraySlider.prototype.collapse = function () {
-  this.$tray.slideUp();
+  this.$tray.animate({
+    'left': this.width * -1
+  });
+  $('body').animate({
+    'padding-left': 0
+  });
   this.state = 'closed';
 };
 /**
  *
  */
 Drupal.TraySlider.prototype.displace = function (event) {
-  this.$tray.css('top', this.computeOffsetTop());
+  this.$tray.css({
+    'top': this.computeOffsetTop()
+  });
 };
 /**
  * Sum all [data-offset-top] values and cache it.
@@ -200,5 +221,15 @@ Drupal.TraySlider.prototype.computeOffsetTop = function () {
   this.offsetTop = sum;
   return sum;
 };
+/**
+ *
+ */
+Drupal.TraySlider.prototype.getWidth = function (event) {
+  var maxClient = document.documentElement.clientWidth;
+  var candidate = maxClient * 0.9;
+  var width = (candidate > this.maxWidth) ? this.maxWidth : candidate;
+  return width;
+};
+
 
 })(jQuery);
