bb24b774c99794c5b48f0faa514d372ea004a80b
J. Renée Beach <splendidnoise@gmail.com>
2012-10-16 21:38:41 -0400

    Issue #1137920 by jessebeach: Finished the first draft of the MediaQueryGroup object.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- core/misc/mediaquery.js
+++ core/misc/mediaquery.js
@@ -2,9 +2,6 @@
 
 "use strict";
 
-var queries = {};
-var attached = false;
-
 Drupal.behaviors.mediaQueryGroup = {
   attach: function (context, settings) {
     $(window).on({
@@ -13,61 +10,88 @@ Drupal.behaviors.mediaQueryGroup = {
   }
 };
 
-function MediaQueryGroup (mq) {
-  var callbacks;
-  // If the supplied mq is a property of queries, just return queries[mq].
-  var query = mq && queries[mq];
-  if (!query) {
-    callbacks = $.Callbacks('unique');
-    query = {
-      query: mq,
-      publish: function () {
-        callbacks.fireWith(this, arguments);
-      },
-      subscribe: function () {
-        callbacks.add.apply(this, arguments);
-        // If the media query applies when the callbacks are added, fire them.
-        if (Drupal.MediaQueryGroup.testMediaQuery(this.query)) {
-          for (var i = 0; i < arguments.length; i++) {
-            if (typeof arguments[i] === 'function') {
-              // The callback might be a bound function, so don't change the
-              // context. Just call it.
-              arguments[i]();
-            }
-          }
-        }
-      },
-      unsubscribe: callbacks.remove
-    };
-    if (mq) {
-      queries[mq] = query;
-    }
+function MediaQueryGroup (namespace) {
+  if (!namespace) {
+    throw new Error("MediaQueryGroup: a namespace must be provided to create a MediaQueryGroup instance.");
   }
-  return query;
+  this.namespace = namespace;
+  Drupal.MediaQueryGroup.groups[namespace] = this;
 }
 /**
  * Utility functions for the MediaQueryGroup object.
  */
 $.extend(MediaQueryGroup, {
-  queries: {},
-  listQueries: function () {
+  fallback: 'default',
+  groups: {},
+  queries: [],
+  list: function () {
     return this.queries;
   },
-  testMediaQuery: function (query) {
-    return this.matchMedia(this.query).matches;
+  test: function (query) {
+    return matchMedia(query).matches;
   },
   refresh: function (event) {
-    var query;
-    for (query in this.queries) {
-      if (this.queries.hasOwnProperty(query) && Drupal.MediaQueryGroup.testMediaQuery(query)) {
-        this.queries[query].publish();
+    var refreshed = [];
+    var query, group;
+    for (var i = 0; i <this.queries.length; i++) {
+      query = this.queries[i];
+      if (this.test(query)) {
+        for (group in this.groups) {
+          if (this.groups.hasOwnProperty(group) && (query in this.groups[group].queries)) {
+            refreshed.push(group);
+            if (this.groups[group].lastFired !== query) {
+              this.groups[group].queries[query].fire();
+              this.groups[group].lastFired = query;
+            }
+          }
+        }
       }
     }
+    // Run the default callback on any group that wasn't refreshed.
+    var defaulters = _.omit(this.groups, refreshed);
+    for (group in defaulters) {
+      if (defaulters.hasOwnProperty(group) && (this.fallback in defaulters[group].queries) && defaulters[group].lastFired !== this.fallback) {
+        defaulters[group].queries[this.fallback].fire();
+        this.groups[group].lastFired = this.fallback;
+      }
+    }
+  },
+  queryAdd: function (mq) {
+    this.queries.push(mq);
+  },
+  queryRemove: function (mq) {
+    /* Cycle through all this.groups and see if this was the last instance of
+    the mq. If so, remove it from this.queries. */
   }
 });
 
 $.extend(MediaQueryGroup.prototype, {
-
+  namespace: '',
+  queries: {},
+  lastFired: '',
+  add: function (mq, callback) {
+    var callbacks = mq && this.queries[mq];
+    if (!callbacks) {
+      callbacks = this.queries[mq] = $.Callbacks('unique');
+      // Add the mq to the global list of queries.
+      if (mq !== Drupal.MediaQueryGroup.fallback) {
+        Drupal.MediaQueryGroup.queryAdd(mq);
+      }
+    }
+    callbacks.add(callback);
+    // If the media query applies when the callback is added, invoke it.
+    // @TODO the fallback should not be invoked if a valid MQ is registered
+    // with this instance and it applies.
+    if (Drupal.MediaQueryGroup.test(mq) || mq === Drupal.MediaQueryGroup.fallback) {
+      // The callback might be a bound function, so don't change the
+      // context. Just call it.
+      callback();
+      this.lastFired = mq;
+    }
+  },
+  remove: function (callback) {
+    this.callbacks.remove(callback);
+  }
 });
 
 $.extend(Drupal, {'MediaQueryGroup': MediaQueryGroup});
